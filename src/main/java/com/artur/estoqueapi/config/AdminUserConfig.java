df91a5cf74513422ddbea236325cd23fee755346
Artur <arturjorgean@gmail.com>
2025-01-13 23:41:51 -0300

    feat: add basic authentication and login endpoint

---
+++ src/main/java/com/artur/estoqueapi/config/AdminUserConfig.java
@@ -0,0 +1,2 @@
+package com.artur.estoqueapi.config;public class AdminUserConfig {
+}
---
+++ src/main/java/com/artur/estoqueapi/controllers/security/TokenController.java
@@ -0,0 +1,50 @@
+package com.artur.estoqueapi.controllers.security;
+
+import com.artur.estoqueapi.domain.dto.security.LoginRequestDto;
+import com.artur.estoqueapi.domain.dto.security.LoginResponseDto;
+import com.artur.estoqueapi.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+
+@RestController
+@AllArgsConstructor
+public class TokenController {
+
+    private JwtEncoder jwtEncoder;
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @PostMapping
+    public ResponseEntity<LoginResponseDto> login (@RequestBody LoginRequestDto loginRequestDto) {
+        var user = userRepository.findByUsername(loginRequestDto.username());
+
+        if (user.isEmpty() || !user.get().isLoginCorrect(loginRequestDto, bCryptPasswordEncoder)) {
+            throw new BadCredentialsException("User or password is not valid.");
+        }
+
+        Instant now = Instant.now();
+        long expiresIn = 300L;
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("EstoqueAPI")
+                .subject(user.get().getUserId().toString())
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiresIn))
+                .build();
+
+        String jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
+        return ResponseEntity.ok(new LoginResponseDto(jwtValue, expiresIn));
+    }
+}
