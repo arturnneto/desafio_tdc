f900024b1dab19f77cdcc75f01a12c4b023b45ef
Artur <arturjorgean@gmail.com>
2025-01-16 13:09:39 -0300

    feat: add create user endpoint

---
+++ src/main/java/com/artur/estoqueapi/controllers/stock/UserController.java
@@ -0,0 +1,55 @@
+package com.artur.estoqueapi.controllers.stock;
+
+import com.artur.estoqueapi.domain.dto.stock.UserCreationDto;
+import com.artur.estoqueapi.domain.entities.auth.RoleEntity;
+import com.artur.estoqueapi.domain.entities.auth.UserEntity;
+import com.artur.estoqueapi.repositories.RoleRepository;
+import com.artur.estoqueapi.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+import java.util.Set;
+
+@RestController
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Transactional
+    @PostMapping("/users")
+    public ResponseEntity<Void> createNewUser(@RequestBody UserCreationDto newUserDto) {
+        var userRole = roleRepository.findByRoleName(RoleEntity.Values.BASIC.name().toLowerCase());
+        Optional<UserEntity> userFromDatabase = userRepository.findByUsername(newUserDto.username());
+
+        if (userFromDatabase.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        UserEntity newUser = new UserEntity();
+        newUser.setUsername(newUserDto.username());
+        newUser.setPassword(bCryptPasswordEncoder.encode(newUserDto.password()));
+        newUser.setRoles(Set.of(userRole));
+
+        userRepository.save(newUser);
+
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ src/main/java/com/artur/estoqueapi/domain/dto/stock/UserCreationDto.java
@@ -0,0 +1,4 @@
+package com.artur.estoqueapi.domain.dto.stock;
+
+public record UserCreationDto(String username, String password) {
+}
