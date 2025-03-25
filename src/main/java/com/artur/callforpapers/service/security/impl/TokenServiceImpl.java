e3ad97b40d61bfa44308c8cc8f459c5aea92c9f2
Artur <arturjorgean@gmail.com>
2025-03-25 17:45:19 -0300

    fix: correct logic for checkifloginiscorrect method

--- src/main/java/com/artur/callforpapers/service/security/impl/TokenServiceImpl.java
+++ src/main/java/com/artur/callforpapers/service/security/impl/TokenServiceImpl.java
@@ -32,7 +32,7 @@ public class TokenServiceImpl implements TokenService {
     public void checkIfLoginIsCorrect(LoginRequestDto loginRequestDto, BCryptPasswordEncoder bCryptPasswordEncoder) {
         Optional<UserEntity> userFromDatabase = userService.getUserLoginEntity(loginRequestDto);
 
-        if (!userFromDatabase.get().isLoginCorrect(loginRequestDto, bCryptPasswordEncoder)) {
+        if (userFromDatabase.isEmpty() || !userFromDatabase.get().isLoginCorrect(loginRequestDto, bCryptPasswordEncoder)) {
             throw new BadCredentialsException("User or password is not valid.");
         }
     }
