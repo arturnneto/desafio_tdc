4c1894ac195091a54c467275d5d1dd1fb5874af2
Artur <arturjorgean@gmail.com>
2025-03-25 23:32:08 -0300

    fix: add repository anottation

--- src/main/java/com/artur/callforpapers/repositories/papers/TalkProposalRepository.java
+++ src/main/java/com/artur/callforpapers/repositories/papers/TalkProposalRepository.java
@@ -2,9 +2,11 @@ package com.artur.callforpapers.repositories.papers;
 
 import com.artur.callforpapers.domain.entities.papers.TalkProposalEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface TalkProposalRepository extends JpaRepository<TalkProposalEntity, Long> {
     Optional<TalkProposalEntity> findByTitle(String title);
 
